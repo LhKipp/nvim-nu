f8231a6955b5986ff33325e2c1f1f55bab1532bc
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-05-04 01:16:39 +0200

    Fix branch used when installing grammar

--- lua/nu/tree_sitter_config.lua
+++ lua/nu/tree_sitter_config.lua
@@ -2,7 +2,8 @@ local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.nu = {
   install_info = {
     url = "https://github.com/LhKipp/tree-sitter-nu",
-    files = {"src/parser.c"}
+    files = {"src/parser.c"},
+    branch = "main"
   },
   filetype = "nu"
 }

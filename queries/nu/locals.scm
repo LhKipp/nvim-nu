c62b00eaee63a23dac4bd4e668b6f450b437d4b2
Tobias Walle <tobias.walle@maibornwolff.de>
2022-11-01 21:56:55 +0100

    Copy latest queries from tree-sitter-nu

--- queries/nu/locals.scm
+++ queries/nu/locals.scm
@@ -1,22 +1,13 @@
 ; Scopes
-
-(function_definition @scope)
-(block @scope)
-(record_or_block @scope)
+(function_definition) @scope
 
 ; Definitions
-(variable_declaration name: (identifier) @definition.var)
-(parameter (identifier) @definition.var)
-(flag (flag_name) @definition.var)
-(flag (flag_shorthand_name) @definition.var)
-(record_entry entry_name: (identifier) @definition.var)
-(block_args block_param: (identifier) @definition.var)
+(variable_declaration 
+  name: (identifier) @definition.var)
 
 (function_definition
   func_name: (identifier) @definition.function)
 
-
 ; References
-[
-    (value_path)
-] @reference
+(value_path) @reference
+(word) @reference

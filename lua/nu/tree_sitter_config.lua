04ad2002173f450ccc5d639b5940c8eb3ee3b0ba
Leonhard Kipp <leonhard.kipp@web.de>
2023-01-03 19:24:43 +0100

    Modernize plugin nu.vim

--- lua/nu/tree_sitter_config.lua
+++
@@ -1,9 +0,0 @@
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.nu = {
-  install_info = {
-    url = "https://github.com/LhKipp/tree-sitter-nu",
-    files = {"src/parser.c", "src/scanner.c"},
-    branch = "main"
-  },
-  filetype = "nu"
-}
--- plugin/nu.vim
+++
@@ -1,1 +0,0 @@
-lua require('nu.tree_sitter_config')

b9742301d1d8822bea9205de4db26929f8bb8ff2
Patrick Jörg <patrickjoerg@gmx.ch>
2023-02-09 09:27:03 +0100

    Downstream change to support own treesitter

--- plugin/nu.lua
+++ plugin/nu.lua
@@ -1,7 +1,7 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.nu = {
     install_info = {
-        url = "https://github.com/LhKipp/tree-sitter-nu",
+        url = "https://github.com/petrisch/tree-sitter-nu",
         files = { "src/parser.c", "src/scanner.c" },
         branch = "main"
     },

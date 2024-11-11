173233bb9bc394e669b71efafbecb82b4e9ecd6f
Idan Arye <IdanArye@gmail.com>
2024-11-11 11:37:20 +0200

    Don't override the tree-sitter grammar repository - let nvim-treesitter use the official one

--- plugin/nu.lua
+++ plugin/nu.lua
@@ -1,12 +1,5 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.nu = {
-    install_info = {
-        url = "https://github.com/LhKipp/tree-sitter-nu",
-        files = { "src/parser.c", "src/scanner.c" },
-        branch = "main"
-    },
-    filetype = "nu"
-}
+parser_config.nu.filetype = "nu"
 
 vim.filetype.add({
     extension = {

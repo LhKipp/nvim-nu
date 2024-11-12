a0c6b2aec65517bd2e5ff0e3364e53bbe93673d3
Idan Arye <IdanArye@gmail.com>
2024-11-12 17:13:59 +0200

    Manually set the revision for the tree-sitter grammar repository
    Otherwise nvim-treesitter will use the revision from its lockfile, which
    uses a different repository with a different implementation of the
    grammar.

--- plugin/nu.lua
+++ plugin/nu.lua
@@ -3,7 +3,8 @@ parser_config.nu = {
     install_info = {
         url = "https://github.com/LhKipp/tree-sitter-nu",
         files = { "src/parser.c", "src/scanner.c" },
-        branch = "main"
+        branch = "main",
+        revision = "ef943c6f2f7bfa061aad7db7bcaca63a002f354c",
     },
     filetype = "nu"
 }
